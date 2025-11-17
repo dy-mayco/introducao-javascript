ba2b6acc7c6892a5eb75abb52fccb81f67258045
dy-mayco <dyony.rodrigues@escola.pr.gov.br>
2025-11-17 11:08:07 -0300

    Criar funções para o form

--- js/form.js
+++ js/form.js
@@ -3,12 +3,34 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(){
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    //Extrai informações do paciente do form
+    var paciente = obtemPacienteDoFormulário(form);
 
-    var pacienteTr = document.createElement("tr");
+    //criar a Tr e a td do paciente
+    var pacienteTr = montaTr(paciente);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+});
+
+// Dividir em tres funções para trabalhar com tarefas menores
+function obtemPacienteDoFormulário(form){
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+    return paciente;
+}
+
+ function montaTr(paciente){
+
+ var pacienteTr = document.createElement("tr");
+
     var nomeTd = document.createElement("td");
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
@@ -19,14 +41,11 @@ botaoAdicionar.addEventListener("click", function(){
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
+    imcTd.testContent = calculaImc(peso,altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-});
+    pacienteTr.appendChild(imcTd);
+ }
