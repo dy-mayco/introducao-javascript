dd7820d2590177dd3ca107e648e6de33f573a117
dy-mayco <dyony.rodrigues@escola.pr.gov.br>
2025-11-19 11:36:16 -0300

    finalizar Tr-Td

--- js/form.js
+++ js/form.js
@@ -49,3 +49,17 @@ function obtemPacienteDoFormulário(form){
     pacienteTr.appendChild(gorduraTd);
     pacienteTr.appendChild(imcTd);
  }
+
+    function montaTd(paciente) {
+        var pacienteTr = document.createElement("tr");
+        pacienteTr.classList.add("paciente");
+
+        pacienteTr.appendChild(montaTd(paciente.nome,"info-nome"));
+        pacienteTr.appendChild(montaTd(paciente.nome,"info-peso"));
+        pacienteTr.appendChild(montaTd(paciente.nome,"info-altura"));
+        pacienteTr.appendChild(montaTd(paciente.nome,"info-gordura"));
+        pacienteTr.appendChild(montaTd(paciente.nome,"info-imc"));
+
+        return pacienteTr;
+
+    }
